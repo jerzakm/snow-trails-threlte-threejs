ef3f3c83c262d0036ad210804ebe255efbac1c61
jerzakm <jerzakm@gmail.com>
2023-02-14 21:53:50 +0100

    cleanup old garbage

--- src/routes/snow/util.ts
+++
@@ -1,8 +0,0 @@
-import type { RigidBody as TRigidBody, Collider as TCollider } from '@dimforge/rapier3d-compat';
-
-export interface Ball {
-	startingPosition: { x: number; y: number; z: number };
-	size: number;
-	rigidBody?: TRigidBody;
-	collider?: TCollider;
-}
